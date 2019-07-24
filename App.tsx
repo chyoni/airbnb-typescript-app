4cfa7cec796d7e8755e62195a17e9432b597c2c2
Chiwon <chiwon99881@gmail.com>
2019-07-24 16:54:31 +0900

    PreLoad Part 1

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+  const preLoad = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font,
+      ...FontAwesome.font
+    });
+    await Asset.loadAsync([
+      require("./assets/noPhoto.jpg"),
+      require("./assets/logo.png")
+    ]);
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded ? (
     <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>loading success</Text>
     </View>
+  ) : (
+    <AppLoading />
   );
 }
